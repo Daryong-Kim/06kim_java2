47ae260617ab0a31219835baa7b2415e3e2a2778
DESKTOP-T2QD4LG\daryong <daryong0905@gmail.com>
2017-09-15 22:46:52 +0900

    課題２はじめ

---
+++ DBLesson02/DBLesson02.java
@@ -0,0 +1,53 @@
+package DBLesson02;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import DBLesson01.Word;
+import DBLesson01.WordDAO;
+
+public class DBLesson02 {
+
+	@SuppressWarnings("resource")
+	public static void main(String[] args) {
+		// Wordクラスのインスタンス配列
+		ArrayList<Word> words = new ArrayList<Word>();
+		WordDAO wordDAO = new WordDAO();
+
+		System.out.println("英単語と日本語をスペースで区切って入力して下さい。");
+		Scanner sc  = new Scanner(System.in);
+
+		int index = 0;
+		try{
+		while(true){
+			String input = sc.nextLine();
+			if(input.equals("e")){
+				break;
+			}
+			String[] tmp = new String[2];
+			tmp = input.split(" ");
+			Word wd = new Word(tmp[0], tmp[1]);
+			words.add(wd);
+			index++;
+
+			/*wordDAO.setBoth(words.get(index));*/
+			/*System.out.println(words);*/
+			System.out.println("次の単語を入力してください。\"e\"で終了します。");
+		}
+
+		}
+		catch(ArrayIndexOutOfBoundsException e){
+			e.printStackTrace();
+		}
+
+		wordDAO.resistWords(words);
+
+		/*for(Word wd : words){
+			System.out.println(wd);
+		}*/
+
+		System.out.println(
+				index
+				+"件登録しました。");
+	}
+}
